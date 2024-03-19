3752155248d7677e04c5a1392518e2e818bfaeac
Domi <domii.hole@gmail.com>
2024-03-19 02:14:08 +0100

    Start des Skripts

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+-- Hier kannst du den Debug Modus an und ausstellen
+Config.Debug = true 
+
+Config.DiscordDebug = false
+Config.DebugWebhook = ""
+
+Config.LogDebug = true
+Config.LogModules = ""
+
+------------
+---- Standard Einstellungen
+------------
+
